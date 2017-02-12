fc0186c37770c184382954ac8a7751b21b23db7c
Martin O Reistadbakk <martin@blaastolen.com>
2017-02-12 01:39:03 +0100

    update batch script to regenerate

--- src/collective/googlecloud/batch.py
+++ src/collective/googlecloud/batch.py
@@ -7,17 +7,19 @@ from zope.site.hooks import setSite
 from AccessControl.SecurityManagement import newSecurityManager
 from plone.dexterity.interfaces import IDexterityItem
 from plone.scale.storage import AnnotationStorage
-
+from zExceptions import NotFound
 def reset_scales(app, args):
 
     parser = argparse.ArgumentParser(description='Reset all scales in the application')
     parser.add_argument('--site', help='Add the site id', required=True)
+    parser.add_argument('--regenerate', help='Scale(s) you want to regnerate, multiple allowed', action='append')
     parser.add_argument('-c', help='stupid bug')
     args = parser.parse_args(args)
     site_name = args.site
 
     root = makerequest.makerequest(app)
     site = root.get(site_name, None)
+
 
 
     if site is None:
@@ -34,8 +36,21 @@ def reset_scales(app, args):
     newSecurityManager(None, user)
     catalog = site.portal_catalog
     for item in catalog(object_provides=IDexterityItem.__identifier__):
-        storage = AnnotationStorage(item.getObject())
+        o = item.getObject()
+        storage = AnnotationStorage(o)
         storage.clear()
+        print "Cleared storage for %s" % (item.getURL())
+        try:
+            for scale in args.regenerate:
+                if hasattr(o,'image'):
+                    scaler = o.unrestrictedTraverse('@@images')
+                    if scaler.scale(fieldname='image', scale=scale) is not None:
+                        print "regenerated scale  %s" % (scale)
+                    else:
+                        print "error regenerating scale  %s" % (scale)
+        except AttributeError:
+            continue
+
     transaction.commit()
     
     
